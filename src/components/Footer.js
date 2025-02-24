523ace15e95e9011d4c83174d70ce90df0abd83f
rajesh_windows <rajesh2121t@gmail.com>
2025-02-24 08:17:27 +0530

    structure of webpage using html and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,44 @@
 import React from 'react';
-
+import footer from '../assets/restauranfood.jpg';
 const Footer = () => {
   return (
     <>
     <div className='footer'>
-      <img></img>
+    <div className='footer-item'>
+      <img src={footer} width="100%"/>
+    </div>
+
+    <div className='footer-item'>
       <h1>Doormat Navigation</h1>
       <div>
-        <p>
-Home<br/>
-About<br/>
-menu<br/>
-Reservations<br/>
-Order Online<br/>
-Login<br/>
-        </p>
-      </div>
-      <h1>Contact</h1>
-      <div>
-        <p>
-Adress<br/>
-phone number<br/>
-email<br/>
-        </p>
-      </div>
-      <h1>Social Media Links</h1>
-      <div>
-        <p>
-Adress<br/>
-phone number<br/>
-email<br/>
-        </p>
+        <button>Home</button><br/>
+        <button>About</button><br/>
+        <button>menu</button><br/>
+        <button>Reservations</button><br/>
+        <button>Order Online</button><br/>
+        <button>Login</button><br/>
       </div>
     </div>
+
+    <div className='footer-item'>
+      <h1>Contact</h1>
+      <div>
+        <button>Address</button><br/>
+        <button>phone number</button><br/>
+        <button>menu</button><br/>
+      </div>
+    </div>
+
+    <div className='footer-item'>
+      <h1>Social Media Links</h1>
+      <div>
+        <button>Address</button><br/>
+        <button>phone number</button><br/>
+        <button>menu</button><br/>
+      </div>
+    </div>
+
+  </div>
     </>
   );
 };
